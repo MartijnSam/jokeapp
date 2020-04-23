dc98b949f791ab439110eba234b63d280b1a66dd
MartijnSam <martijnsamuels@gmail.com>
2020-04-23 14:57:38 +0200

    added hide/show button for answer of joke

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const capitalize = (s) => {
     return s.charAt(0).toUpperCase() + s.slice(1)
   }
 
+
 const app = express()
 const port = 3000
 function onListen(){
     console.log(`Listening on :${port}`)
 }
 
-const jokes = {joke1: 'joke1', joke2: 'joke2', joke3:'joke3', joke4:'joke4', joke5:'joke5'}
+const jokes = [{joke:"What's a Dutch party without bitterballen?", answer: "A worst kaas scenario", image:'<img src="https://www.1limburg.nl/sites/default/files/public/styles/media-paragraph/public/kaasenworst.jpg?itok=eM_geNQr" alt="worst kaas scenario">'},
+{joke: 'What’s brown and sticky?', answer: "A stick", image:'<img src="images/stick.pgn" alt="just a brown stick"'},
+{joke:'joke3'},{joke:'joke4'},{joke:'joke5'}]
 
 console.log(jokes)
 
@@ -24,19 +27,35 @@ const rhymnow = rhyming(request.params.name)
 const rhymewords = rhymnow.map(function(word){
     return word.word
 })
-console.log(rhymewords)
 
 const rhymeblock = function(rhymewords){
 if (rhymewords.length <= 0) 
     return `doesnt seem to rhyme with any English words...` 
 else return `rhymes with:</p>
-<p><dev id=rhymewords><ul><li>${rhymewords.join('</li><li>')}</li></ul>`}
+<p><div id=rhymewords><ul><li>${rhymewords.join('</li><li>')}</li></ul>`}
 const rhymetext = rhymeblock(rhymewords)
-console.log(rhymetext)
+const jokeblock = function() {
+    return`<div id="joke">
+<p><h2>${jokes[0].joke}</h2> <button onclick="hideShow()">Click Me</button></p>
+<div id="answer" style="display:none">
+<p><h2>${jokes[0].answer}</h2></p>
+<p>${jokes[0].image}</p>
+</div>
+</div>`
+}
 
-response.send(`<html><head><title>Jokester Pro 6000</title></head><body><h1>Welcome</h1><body>
+response.send(`<html><head><title>Jokester Pro 6000</title><script>
+function hideShow() {
+    var x = document.getElementById("answer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }</script></head><body><h1>Welcome</h1><body>
 <p>Hello ${capitalize(request.params.name)},</p>
-<dev id=rhyme><p>In case you didn't knew, your name ${capitalize(request.params.name)}, ${rhymetext}</p></dev></dev>
+<p>${jokeblock()}</p}
+<div id="rhyme"><p>In case you didn't knew, your name ${capitalize(request.params.name)}, ${rhymetext}</p></div></div>
 </body></html>`)})
 
 
